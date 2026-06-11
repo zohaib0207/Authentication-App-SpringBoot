3939c91f916a65ba2471d0a060d17fd4e66c52b3
zohaib0207 <zohaibalam05@hotmail.com>
2026-06-11 13:21:52 +0530

    Initial Spring Boot authentication project setup

---
+++ src/main/java/com/lcwz/auth/Auth_App_Backend/AuthAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lcwz.auth.Auth_App_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcwz/auth/Auth_App_Backend/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwz.auth.Auth_App_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
