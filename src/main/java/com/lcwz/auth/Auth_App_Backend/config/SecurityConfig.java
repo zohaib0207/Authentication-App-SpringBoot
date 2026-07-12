de68451da7eb576bbb6bc827402a7d223910095a
zohaib0207 <zohaibalam05@hotmail.com>
2026-07-13 02:46:50 +0300

    Pre JWT application

--- src/main/java/com/lcwz/auth/Auth_App_Backend/config/SecurityConfig.java
+++ src/main/java/com/lcwz/auth/Auth_App_Backend/config/SecurityConfig.java
@@ -3,11 +3,30 @@ package com.lcwz.auth.Auth_App_Backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable(); //Disabling CSRF protection for simplicity, but in production, you should enable it
+        http.authorizeHttpRequests(authorizeHttpRequests ->
+                authorizeHttpRequests
+                        .requestMatchers("/api/v1/auth/register").permitAll()//Everyone can access this endpoint without authentication
+                        .requestMatchers("/api/v1/auth/login").permitAll() //Accessible without autrhentication
+                        .anyRequest().authenticated() //Remaining endpoints require authentication
+        )
+                .httpBasic(Customizer.withDefaults()); //Enabling basic authentication
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
